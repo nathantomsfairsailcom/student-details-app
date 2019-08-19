ceb664921a73ace21694b5835c94a929171211b5
Nathan Toms <nathan.toms@sage.com>
2019-08-19 11:07:35 +0100

    Routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components';
-import { SuccessPageComponent } from './pages';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/components";
+import { FormPageComponent, SuccessPageComponent } from "./pages";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    FormPageComponent,
     SuccessPageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
