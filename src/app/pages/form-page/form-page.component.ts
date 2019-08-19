89a48464b6e4233cf736d971a1233e04cff9e8ea
eddiesummers@sagecom <eddie.summers@sage.com>
2019-08-19 13:51:38 +0100

    Add service to form page; update deploy script

--- src/app/pages/form-page/form-page.component.ts
+++ src/app/pages/form-page/form-page.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+// Angular
+import { Component, Inject } from '@angular/core';
+
+// Services
+import { DetailsService } from 'src/app/services';
 
 @Component({
   selector: 'sd-form-page',
   templateUrl: './form-page.component.html',
   styleUrls: ['./form-page.component.scss']
 })
-export class FormPageComponent implements OnInit {
-  constructor() {}
+export class FormPageComponent {
+  constructor(@Inject(DetailsService) private detailsService: DetailsService) {}
 
-  ngOnInit() {}
+  /**
+   * Send details from the form to the backend.
+   */
+  addDetails(): void {
+    const details = {potato: 'King Edward'};
+
+    this.detailsService.addDetails(details).subscribe(
+
+      (result: any) => {
+        console.log(result);
+      },
+
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
