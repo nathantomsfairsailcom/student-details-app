20a17faa29be6432c8ccc163bc12a2f2cded1d5a
eddiesummers@sagecom <eddie.summers@sage.com>
2019-08-19 15:41:42 +0100

    Implement real save method in Functions; add docs

--- docs/Firebase.md
+++ docs/Firebase.md
@@ -3,3 +3,7 @@
 [Quickstart Guide](https://firebase.google.com/docs/hosting/quickstart)
 
 [Cloud Functions (Backend)](https://firebase.google.com/docs/hosting/functions)
+
+[End-to-End Cloud Functions](https://firebase.google.com/docs/functions/get-started)
+
+[Cloud Functions and Firestore](https://cloud.google.com/community/tutorials/cloud-functions-firestore)
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,16 @@
+//==============================================================================
+// Environment
+//==============================================================================
+
 // Firebase
 const functions = require('firebase-functions');
+
+// Firestore
+const Firestore = require('@google-cloud/firestore');
+const firestore = new Firestore({
+  projectId: 'student-details-app-sgp',
+  timestampsInSnapshots: true,
+});
 
 /*
  * CORS middleware required because the hosting domain and Cloud Functions domain
@@ -9,16 +20,51 @@ const functions = require('firebase-functions');
  */
 const cors = require('cors')({origin: true});
 
+/*
+ * This is currently a "test" collection, which means anyone with the URL can
+ * make the below request without authenticating. In production we would want to
+ * use a private collection, which would require some authentication in here.
+ */
+const studentsTable = 'students';
+
 //==============================================================================
 // Endpoints
 //==============================================================================
 
+/**
+ * Save a new Student to the Students collection.
+ */
 exports.save = functions.https.onRequest((req, res) => {
-  // TODO save something
 
-  return cors(req, res, () => {
-    res.status(201).send({
-      message: 'Details saved successfully'
+  // Deny forbidden request methods
+  if (req.method === 'GET' || req.method === 'PUT' || req.method === 'DELETE') {
+    return cors(req, res, () => {
+      res.status(403).send(
+        {error: ('Could not perform ' + req.method + ' operation.')}
+      );
     });
-  });
+  }
+
+  // Get details from request
+  const newRecord = {
+    name: req.body.name,
+    email: req.body.email,
+    degreeTitle: req.body.degreeTitle,
+    currentYearOfStudy: req.body.currentYearOfStudy
+  };
+
+  // Add to database
+  return firestore.collection(studentsTable)
+    .add(newRecord)
+    .then(() => {
+      return cors(req, res, () => {
+        res.status(201).send({message: 'Details saved successfully.'});
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      return cors(req, res, () => {
+        res.status(500).send({message: 'Unable to store details.', error: err});
+      });
+    });
 });
