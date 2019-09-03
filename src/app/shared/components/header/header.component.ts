edca94a1314004521235cf7cc918c4d3dee5b33f
Nathan Toms <nathan.toms@sage.com>
2019-09-03 09:58:04 +0100

    Added logout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services';
 
 @Component({
   selector: 'sd-header',
@@ -8,9 +9,33 @@ import { Component } from '@angular/core';
         <a class="navbar-brand" href="#">
           <img src="assets/images/sage-logo.svg" height="30" class="d-inline-block align-top" alt="">
         </a>
+        <span class="logout navbar-text" *ngIf="authService.isLoggedIn" (click)="logOut()">
+          Logout
+        </span>
       </div>
     </nav>
   `,
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+
+  isLoggedIn: boolean = false;
+
+  constructor(
+    public authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.setLoggedIn();
+  }
+
+  logOut() {
+    this.authService.logout();
+    this.setLoggedIn();
+  }
+
+  private setLoggedIn() {
+    this.isLoggedIn = this.authService.isLoggedIn;
+  }
+
+}
